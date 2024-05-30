992e7b3753ff48b0d58c03a5928a568ee71c5aad
mathiasg12 <mathiasgausl@gmail.com>
2024-05-30 10:08:50 +0200

    created project cards

--- src/components/RecentProjectCard/index.jsx
+++ src/components/RecentProjectCard/index.jsx
@@ -1,6 +1,50 @@
+import { Link } from 'react-router-dom';
+import styles from './recentProjectCard.module.css';
+
 /**
  * component that creates the "Recent project card" this card contains a image, title, short decription and links to the live site and github repository
  */
 export function RecentProjectCard(props) {
   const { projectArray } = props;
+  return (
+    <div>
+      {projectArray.map((eachObject) => {
+        return (
+          <div className={styles.card}>
+            <div className={styles.imageDiv}>
+              <img src={eachObject.imageUrl} alt="screenshot of project" />
+            </div>
+            <h3>{eachObject.title}</h3>
+            <p className={styles.desc}>{eachObject.decription}</p>
+            <div>
+              <h4>Built with</h4>
+              <div className={styles.bultWithCon}>
+                {eachObject.builtWith.map((item) => {
+                  return (
+                    <div className={styles.svgCon}>
+                      <p>{item.name}</p>
+                      <div>
+                        <img src={item.svg} alt="svg of tool" />
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+            <div className={styles.linkCon}>
+              <Link to={eachObject.liveSite} className={styles.link}>
+                Live site
+              </Link>
+              <Link
+                to={eachObject.gitHub}
+                className={`${styles.link} ${styles.gitHubLink}`}
+              >
+                Github repo
+              </Link>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
--- src/components/RecentProjectSection/index.jsx
+++ src/components/RecentProjectSection/index.jsx
@@ -1,8 +1,11 @@
+import { recentProjectArray } from '../../JS/recentProjectArray';
+import { RecentProjectCard } from '../RecentProjectCard';
 import styles from './recentProject.module.css';
 export function RecentProjects() {
   return (
     <section className={styles.recentProjectsSection}>
       <h1>Recent projects</h1>
+      <RecentProjectCard projectArray={recentProjectArray}></RecentProjectCard>
     </section>
   );
 }
